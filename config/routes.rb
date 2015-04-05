85ae9303c96b5b0a36c70318cc2e5d0258f6969f
Jayklutch <klutchdacar@outlook.com>
2015-04-05 22:55:00 +1200

    Its working, there's a weird sensation inside me, I like it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-   root 'home#index'
+  authenticated :user do
+ root to: "home#index", as: :authenticated_root
+end
+
+
 
 get '/control', to: 'home#control'
 get '/monitor', to: 'home#monitor'
@@ -9,4 +13,10 @@ get '/cams', to: 'home#cams'
 get '/traffic', to: 'home#traffic'
 
 patch '/items/:id/toggle', to: 'items#toggle'
+
+unauthenticated do
+  devise_scope :user do
+    root to: "devise/registrations#new", as: :unauthenticated_root
+  end
+end
 end
