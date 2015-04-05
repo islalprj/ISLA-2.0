abf1cab7a2fd77a8f746ef8efdd1dcfdc0f0ebdd
Jayklutch <klutchdacar@outlook.com>
2015-04-05 22:37:30 +1200

    added 4th and 5th page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 
    root 'home#index'
 
-patch '/items/:id/toggle', to: 'items#toggle'
-
 get '/control', to: 'home#control'
 get '/monitor', to: 'home#monitor'
 get '/cams', to: 'home#cams'
 get '/traffic', to: 'home#traffic'
 
+patch '/items/:id/toggle', to: 'items#toggle'
 end
