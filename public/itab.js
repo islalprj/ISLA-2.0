8439069881779e9369adb6a2e68406a47b99b1a5
philldellow <philldellow@gmail.com>
2015-04-07 00:01:40 +1200

    converted to vanilla javascript. established color palete

--- public/itab.js
+++ public/itab.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
+window.readyHandlers = [];
+window.ready = function ready(handler) {
+  window.readyHandlers.push(handler);
+  handleState();
+};
+
+window.handleState = function handleState () {
+  if (['interactive', 'complete'].indexOf(document.readyState) > -1) {
+    while(window.readyHandlers.length > 0) {
+      window.readyHandlers.shift()();
+    }
+  }
+};
+
+document.onreadystatechange = window.handleState;
+ready(function () {
+
 	setTimeout(function(){
 	document.getElementById('fountainG').style.display = "none";
-	},7500);
+	},10000);
 })
