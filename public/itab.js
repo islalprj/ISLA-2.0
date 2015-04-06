b37dcab00f173e47805e22fe23dad2300f6f266b
philldellow <philldellow@gmail.com>
2015-04-07 02:01:45 +1200

    assembled and arranged the tricky little frames on the edgethat will roll out n over spreading goodness and light over the top of the isla

--- public/itab.js
+++ public/itab.js
@@ -18,4 +18,11 @@ ready(function () {
 	setTimeout(function(){
 	document.getElementById('fountainG').style.display = "none";
 	},10000);
+	setTimeout(function(){
+	document.getElementById('controller').style.display = "block";
+	document.getElementById('weather').style.display = "block";
+	document.getElementById('traffic').style.display = "block";
+	document.getElementById('cams').style.display = "block";
+	},10005);
+	
 })
