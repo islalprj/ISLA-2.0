dd08c7981fbbe6954893bd5f11ee4458932ea9f6
Joshua Vial <joshua@enspiral.com>
2015-04-06 12:21:16 +1200

    setting up omniauth with facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1421547268149102", "73fad9609562720c0709076fd27dcebd"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   authenticated :user do
- root to: "home#index", as: :authenticated_root
-end
-
-
-
-get '/control', to: 'home#control'
-get '/monitor', to: 'home#monitor'
-get '/cams', to: 'home#cams'
-get '/traffic', to: 'home#traffic'
-
-patch '/items/:id/toggle', to: 'items#toggle'
-
-unauthenticated do
-  devise_scope :user do
-    root to: "devise/registrations#new", as: :unauthenticated_root
+   root to: "home#index", as: :authenticated_root
   end
-end
+
+  get '/control', to: 'home#control'
+  get '/monitor', to: 'home#monitor'
+  get '/cams', to: 'home#cams'
+  get '/traffic', to: 'home#traffic'
+
+  patch '/items/:id/toggle', to: 'items#toggle'
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/registrations#new", as: :unauthenticated_root
+    end
+  end
 end
