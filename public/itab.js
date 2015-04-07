dc5e2e5eb7f6479d280d111495e8a5e771b2c467
philldellow <philldellow@gmail.com>
2015-04-08 10:39:01 +1200

    animation from top bottom left and right now flowing divs onto screen

--- public/itab.js
+++ public/itab.js
@@ -17,14 +17,14 @@ ready(function () {
 
 	setTimeout(function(){
 	document.getElementById('fountainG').style.display = "none";
-	},10000);
+	},3000);
 	setTimeout(function(){
 	document.getElementById('controller').style.display = "block";
 	document.getElementById('weather').style.display = "block";
 	document.getElementById('traffic').style.display = "block";
 	document.getElementById('cams').style.display = "block";
 	
-	},10000);
+	},3000);
 
 	console.log('whoa!')
 })
