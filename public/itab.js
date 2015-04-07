d6f9ee7c088967d0cfffc0713e55ccf5a97cb897
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-04-07 22:01:27 +1200

    Deleted the data from all the tabs, and got few snapshots

--- public/itab.js
+++ public/itab.js
@@ -26,3 +26,11 @@ ready(function () {
 	},10005);
 
 })
+
+var expandCollapseApp = angular.module('expandCollapseApp', ['ngAnimate']);
+
+expandCollapseApp.controller('expandCollapseCtrl', function ($scope) {
+        $scope.active = true;
+        $scope.active1 = true;
+
+});
