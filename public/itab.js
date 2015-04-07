bc7f0780a2ae89656ee61fc113f99837b46e026f
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-04-07 20:48:17 +1200

    ?

--- public/itab.js
+++ public/itab.js
@@ -26,3 +26,16 @@ ready(function () {
 	},10005);
 
 })
+
+$(function () {
+    $('.front-page').hover(function () {
+        var pos = $(this).position();
+        var $clone = $(this).clone();
+        $clone.appendTo(this).addClass('front-page_clone').css({
+            left: pos.left,
+            top: pos.top
+        });
+    }, function () {
+        $('.front-page_clone', this).remove();
+    });
+});
