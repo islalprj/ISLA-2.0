2c515a6c0a3ddb5f7af07ff74e6d864e97fe70bc
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-04-08 17:04:26 +1200

    Deleted som merge conflict stuff from itab.js

--- public/itab.js
+++ public/itab.js
@@ -17,30 +17,16 @@ ready(function () {
 
 	setTimeout(function(){
 	document.getElementById('fountainG').style.display = "none";
-<<<<<<< HEAD
-},5000);
-=======
 	},10000);
->>>>>>> bcfca02589a4b99cf6254110a238bc1d00f23e76
 	setTimeout(function(){
 	document.getElementById('controller').style.display = "block";
 	document.getElementById('weather').style.display = "block";
 	document.getElementById('traffic').style.display = "block";
 	document.getElementById('cams').style.display = "block";
-<<<<<<< HEAD
   document.getElementById('baseDiv').style.display = "block";
-
-},5000);
-
-
-
-	console.log('whoa!');
-});
-=======
 	document.getElementById('baseDiv').style.display = "block";
-	
+
 	},10000);
 
 	console.log('whoa!')
 })
->>>>>>> bcfca02589a4b99cf6254110a238bc1d00f23e76
