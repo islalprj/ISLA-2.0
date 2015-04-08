0fd54173249052ebc289676d7fea4f534cd843f2
tojarra <luiglenn@gmail.com>
2015-04-08 16:26:24 +1200

    refactored and tested home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,11 @@
 class HomeController < ApplicationController
   def index
     @items=Item.all.order(:id)
-    Gmail.new("klutch.nz.dev@gmail.com", "Holden93!") do |gmail|
-      @emails=gmail.inbox.find(:unread)
-      zzz=@emails.count
-      @subjects=[1,2,3]
-      @senders=[]
 
-      @senders<<"#{gmail.inbox.emails[zzz-1].sender.first[:name]} Sends: "
-      @senders<<gmail.inbox.emails[zzz-1].subject
-      @senders<<"-----------------------"
-      @senders<<"#{gmail.inbox.emails[zzz-2].sender.first[:name]} Sends: "
-      @senders<<gmail.inbox.emails[zzz-2].subject
-      @senders<<"-----------------------"
-      @senders<<"#{gmail.inbox.emails[zzz-3].sender.first[:name]} Sends: "
-      @senders<<gmail.inbox.emails[zzz-3].subject
-      @senders<<"-----------------------"
-      @senders<<"#{gmail.inbox.emails[zzz-4].sender.first[:name]} Sends: "
-      @senders<<gmail.inbox.emails[zzz-4].subject
-      @senders<<"-----------------------"
-      @senders<<"#{gmail.inbox.emails[zzz-5].sender.first[:name]} Sends: "
-      @senders<<gmail.inbox.emails[zzz-5].subject
+    Gmail.new("klutch.nz.dev@gmail.com", "Holden93!") do |gmail|
+      @emails = gmail.inbox.find(:unread)
+      # @senders = get_senders(gmail, @emails)
+      @senders = PopulateSenders.get_senders(gmail, @emails)
     end
   end
   def control
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,8 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module FetchGmail
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,6 +18,7 @@ module FetchGmail
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.autoload_paths+=%W(#{config.root}/app/models)
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
